971fe3058a71789113e38cf391b01889ec107e52
Parker Ziegler <parkerellisziegler@gmail.com>
2025-01-21 12:57:15 -0800

    fix: Remove Tailwind preflight to avoid altering Global grist styles.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,11 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
 		extend: {}
 	},
-
+	corePlugins: {
+		preflight: false
+	},
 	plugins: []
 } satisfies Config;
